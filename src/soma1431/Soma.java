ccdef495ae57eee46bb20559d927305551224d1e
Nick Taras <driverbulba@gmail.com>
2015-10-27 20:13:28 +0200

    Added application window.

---
+++ src/soma1431/Soma.java
@@ -0,0 +1,43 @@
+package soma1431;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Soma {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Soma window = new Soma();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Soma() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
