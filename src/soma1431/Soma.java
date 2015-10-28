85b4c99b9d2ec1e8bb19d4d52506dd9c22eb6886
NIKOLAS <NIKOLAS@NIKOLAS-PC>
2015-10-28 21:03:44 +0200

    Added Menu row.

--- src/soma1431/Soma.java
+++ src/soma1431/Soma.java
@@ -3,10 +3,22 @@ package soma1431;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JToolBar;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.CardLayout;
 
 public class Soma {
 
 	private JFrame frame;
+
+	
 
 	/**
 	 * Launch the application.
@@ -36,8 +48,33 @@ public class Soma {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 600, 460);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JFrame addPlaylistFrame = new JFrame();
+		addPlaylistFrame.setBounds(100,100,300,230);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+		
+		JMenuItem mntmExit = new JMenuItem("Exit");
+		mnFile.add(mntmExit);
+		
+		JMenu mnAdd = new JMenu("Add");
+		menuBar.add(mnAdd);
+		
+		JMenuItem mntmPlaylist = new JMenuItem("Playlist...");
+		mnAdd.add(mntmPlaylist);
+		
+		JMenu mnHelp = new JMenu("Help");
+		menuBar.add(mnHelp);
+		
+		JMenuItem mntmAbout = new JMenuItem("About");
+		mnHelp.add(mntmAbout);
+		frame.getContentPane().setLayout(new CardLayout(0, 0));
 	}
 
 }
