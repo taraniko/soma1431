eb022b0a084cea41d3483f573ad8d9013fb10405
Nick Taras <driverbulba@gmail.com>
2015-11-04 11:09:53 +0200

    Added jTable with jComboboxes.

--- src/soma1431/Soma.java
+++ src/soma1431/Soma.java
@@ -1,5 +1,6 @@
 package soma1431;
 
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.EventQueue;
 import java.awt.GridBagLayout;
@@ -24,6 +25,10 @@ import java.awt.CardLayout;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
 
 import java.awt.GridBagConstraints;
 import java.io.File;
@@ -65,7 +70,7 @@ public class Soma {
 		//Create and set up the window.
 		JFrame frame = new JFrame();
 		frame = new JFrame();
-		frame.setBounds(100, 100, 500, 360);
+		frame.setBounds(100, 100, 640, 480);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//Create and set up the content pane.
@@ -90,6 +95,7 @@ public class Soma {
         //Create the panel that contains the "cards".
         cards = new JPanel(new CardLayout());
         cards.add(cardTable,"table");
+        
         cards.add(cardPreferences,"preferences");
         
         
@@ -99,9 +105,69 @@ public class Soma {
     }
 	
 	public static void fillTableCard(JPanel cardTable){
-		cardTable.add(new JButton("Button 1"));
-        cardTable.add(new JButton("Button 2"));
-        cardTable.add(new JButton("Button 3"));
+		cardTable.setLayout(new BorderLayout());
+		JTable table = new JTable(new MyTableModel());
+		JScrollPane scrollPane = new JScrollPane(table);
+		initColumnSizes(table);
+		addComboboxAtTable(table);
+		cardTable.add(scrollPane);
+		
+	}
+	
+	public static void initColumnSizes(JTable table){
+		MyTableModel model = (MyTableModel)table.getModel();
+        TableColumn column = null;
+        Component comp = null;
+        int headerWidth = 0;
+        int cellWidth = 0;
+        Object[] longValues = model.longValues;
+        TableCellRenderer headerRenderer =
+            table.getTableHeader().getDefaultRenderer();
+ 
+        for (int i = 0; i < 5; i++) {
+            column = table.getColumnModel().getColumn(i);
+ 
+            comp = headerRenderer.getTableCellRendererComponent(
+                                 null, column.getHeaderValue(),
+                                 false, false, 0, 0);
+            headerWidth = comp.getPreferredSize().width;
+ 
+            comp = table.getDefaultRenderer(model.getColumnClass(i)).
+                             getTableCellRendererComponent(
+                                 table, longValues[i],
+                                 false, false, 0, i);
+            cellWidth = comp.getPreferredSize().width;
+ 
+            column.setPreferredWidth(Math.max(headerWidth, cellWidth));
+        }
+	}
+	
+	
+	
+	/**
+	 * @param table
+	 */
+	public static void addComboboxAtTable(JTable table) {
+		//Set up the editor for the sport cells.
+		JComboBox comboBox = new JComboBox();
+		comboBox.addItem("Rock");
+		comboBox.addItem("Funk");
+		comboBox.addItem("Reggae");
+		comboBox.addItem("Ska");
+		comboBox.addItem("Balkan");
+		comboBox.addItem("Jazz");
+		
+		
+		//Set up tool tips for the sport cells.
+		DefaultTableCellRenderer renderer =
+				new DefaultTableCellRenderer();
+		renderer.setToolTipText("Click to change playlist");
+		for (int i=1; i<=7; i++){
+			TableColumn tempColumn = table.getColumnModel().getColumn(i);
+			tempColumn.setCellEditor(new DefaultCellEditor(comboBox));
+			tempColumn.setCellRenderer(renderer);
+		}
+		
 	}
 	
 	public static void fillPreferencesCard(JPanel cardPreferences){
@@ -241,5 +307,93 @@ class dataFile{
 	
 	
 	
+}
+
+class MyTableModel extends AbstractTableModel {
+    private String[] columnNames = {"  ",
+                                    "Monday",
+                                    "Tuesday",
+                                    "Wednesday",
+                                    "Thursday",
+                                    "Friday",
+                                    "Saturday",
+                                    "Sunday"};
+    private Object[][] data = {
+    {"00:00", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>"},
+    {"01:00", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>"},
+    {"02:00", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>"},
+    {"03:00", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>"},
+    {"04:00", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>"},
+    {"05:00", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>"},
+    {"06:00", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>"},
+    {"07:00", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>"},
+    {"08:00", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>"},
+    {"09:00", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>"},
+    {"10:00", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>"},
+    {"11:00", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>"},
+    {"12:00", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>"},
+    {"13:00", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>"},
+    {"14:00", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>"},
+    {"15:00", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>"},
+    {"16:00", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>"},
+    {"17:00", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>"},
+    {"18:00", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>"},
+    {"19:00", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>"},
+    {"20:00", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>"},
+    {"21:00", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>"},
+    {"22:00", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>"},
+    {"23:00", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>", "<Select>"},
+    };
+
+    public final Object[] longValues = {" 23:00 ", "Psychedelic", "Psychedelic", "Psychedelic", 
+    		"Psychedelic", "Psychedelic", "Psychedelic", "Psychedelic" };
+
+    public int getColumnCount() {
+        return columnNames.length;
+    }
+
+    public int getRowCount() {
+        return data.length;
+    }
+
+    public String getColumnName(int col) {
+        return columnNames[col];
+    }
+
+    public Object getValueAt(int row, int col) {
+        return data[row][col];
+    }
+
+    /*
+     * JTable uses this method to determine the default renderer/
+     * editor for each cell.  If we didn't implement this method,
+     * then the last column would contain text ("true"/"false"),
+     * rather than a check box.
+     */
+    public Class getColumnClass(int c) {
+        return getValueAt(0, c).getClass();
+    }
+
+    /*
+     * Don't need to implement this method unless your table's
+     * editable.
+     */
+    public boolean isCellEditable(int row, int col) {
+        //Note that the data/cell address is constant,
+        //no matter where the cell appears onscreen.
+        if (col < 1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
+    public void setValueAt(Object value, int row, int col) {
+        data[row][col] = value;
+        fireTableCellUpdated(row, col);
+    }
+
+
+
 }
 
